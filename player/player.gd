49df9b6f29b18a7d34cd440a956c033da5725e56
Madi Madden <cinnamon.madi@gmail.com>
2021-07-25 07:47:07 -0500

    RTS movement and selection, camera panning
    Signed-off-by: Madi Madden <cinnamon.madi@gmail.com>

---
+++ player/player.gd
@@ -0,0 +1,133 @@
+extends Camera2D
+
+onready var image_cursor = load("res://player/cursor.png")
+onready var map = get_viewport().get_node("world/nav/map")
+onready var parent = get_parent()
+
+const TILE_SIZE = 32
+const SCREEN_WIDTH = 1280
+const SCREEN_HEIGHT = 720
+const PAN_MARGIN = 15
+const PAN_SPEED = 1000.0
+
+# mouse input
+var mouse_captured = false
+var mouse_pos = Vector2.ZERO
+var mouse_global_pos = Vector2.ZERO
+
+# camera panning
+var min_position
+var max_position
+
+# selecting
+var selecting = false
+var select_start = Vector2.ZERO
+var selected = []
+
+func _ready():
+    current = true
+    set_scroll_bounds()
+    position = min_position
+    mouse_capture()
+
+func set_scroll_bounds():
+    var map_tile_width = 1
+    var map_tile_height = 1
+    while map.get_cell(map_tile_width, 0) != -1:
+        map_tile_width += 1
+    while map.get_cell(0, map_tile_height) != -1:
+        map_tile_height += 1
+
+    min_position = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    max_position = Vector2((map_tile_width * TILE_SIZE) - (SCREEN_WIDTH / 2), (map_tile_height * TILE_SIZE) - (SCREEN_HEIGHT / 2))
+
+func _process(delta):
+    handle_input()
+    if mouse_captured and not selecting:
+        update_camera(delta)
+    if selecting:
+        update()
+
+func _draw():
+    if selecting:
+        draw_rect(Rect2(select_start, mouse_pos - select_start - position), Color(1, 1, 0), false)
+
+func handle_input():
+    if not mouse_captured:
+        if Input.is_action_just_pressed("escape"):
+            get_tree().quit()
+        if Input.is_action_just_pressed("left_click"):
+            mouse_capture()
+        return
+
+    if Input.is_action_just_pressed("escape"):
+        mouse_free()
+
+    mouse_pos = get_viewport().get_mouse_position()
+    mouse_global_pos = get_global_mouse_position()
+    if Input.is_action_just_pressed("left_click"):
+        select_begin()
+    if Input.is_action_just_released("left_click"):
+        select_end()
+
+    if Input.is_action_just_pressed("right_click"):
+        var global_mouse_pos = get_global_mouse_position()
+        for unit in selected:
+            if unit.get_ref():
+                unit.get_ref().navigate_to(global_mouse_pos)
+
+func select_begin():
+    selecting = true
+    select_start = mouse_pos - position
+
+func select_end():
+    selecting = false
+    update()
+
+    for unit in selected:
+        if unit.get_ref():
+            unit.get_ref().deselect()
+    selected = []
+
+    for unit in get_tree().get_nodes_in_group("units"):
+        unit.select_ring.visible = false
+
+    var select_rect = RectangleShape2D.new()
+    select_rect.extents = (mouse_pos - select_start - position) / 2
+    if select_rect.extents.x == 0:
+        select_rect.extents.x = 1
+    if select_rect.extents.y == 0:
+        select_rect.extents.y = 1
+    var select_query = Physics2DShapeQueryParameters.new()
+    select_query.set_shape(select_rect)
+    select_query.transform = Transform2D(0, select_start + position + select_rect.extents)
+    var query_results = get_world_2d().direct_space_state.intersect_shape(select_query, 100)
+    for result in query_results:
+        if result.collider.is_in_group("units"):
+            selected.append(weakref(result.collider))
+
+    for unit in selected:
+        unit.get_ref().select(selected)
+
+func update_camera(delta):
+    if mouse_pos.x <= PAN_MARGIN:
+        position.x -= PAN_SPEED * delta
+    elif mouse_pos.x >= SCREEN_WIDTH - PAN_MARGIN:
+        position.x += PAN_SPEED * delta
+    if mouse_pos.y <= PAN_MARGIN:
+        position.y -= PAN_SPEED * delta
+    elif mouse_pos.y >= SCREEN_HEIGHT - PAN_MARGIN:
+        position.y += PAN_SPEED * delta
+
+    position.x = clamp(position.x, min_position.x, max_position.x)
+    position.y = clamp(position.y, min_position.y, max_position.y)
+
+func mouse_capture():
+    Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+    Input.set_custom_mouse_cursor(image_cursor)
+    mouse_captured = true
+
+func mouse_free():
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+    Input.set_custom_mouse_cursor(null)
+    mouse_captured = false
---
+++ units/tomato.gd
@@ -0,0 +1,92 @@
+extends KinematicBody2D
+
+const SPEED = 120
+const MOVE_TIMEOUT = 0.4
+const MIN_TARGET_DISTANCE = 8
+const NODE_RESET_THRESHOLD = 16
+
+onready var nav = get_viewport().get_node("world/nav")
+onready var rays = $rays
+onready var ray_front = $rays/ray0
+onready var move_timer = $move_timer
+onready var select_ring = $select_ring
+
+var path = []
+var target = null
+var velocity = Vector2.ZERO
+var stuck_position = null
+
+var selected = false
+var select_group = null
+
+func _ready():
+    move_timer.connect("timeout", self, "_on_move_timeout")
+    add_to_group("units")
+
+func _physics_process(_delta):
+    if target != null:
+        move()
+        if (path.empty() and position.distance_to(target) < MIN_TARGET_DISTANCE) or (not path.empty() and position.distance_to(target) < NODE_RESET_THRESHOLD):
+            target = null
+        if target != null and move_timer.is_stopped():
+            handle_colliders()
+
+    set_target()
+
+func move():
+    velocity = position.direction_to(target) * SPEED
+    rays.rotation = velocity.angle()
+    if ray_front.is_colliding():
+        var viable_ray = get_viable_ray()
+        if viable_ray:
+            velocity = Vector2.RIGHT.rotated(rays.rotation + viable_ray.rotation) * SPEED
+    velocity = move_and_slide(velocity)
+
+
+func get_viable_ray():
+    for ray in rays.get_children():
+        if !ray.is_colliding():
+            return ray
+    return null
+
+func handle_colliders():
+    for i in get_slide_count():
+        var collider = get_slide_collision(i).collider
+        var collider_in_select_group = false
+        for unit in select_group:
+            if unit.get_ref() and unit.get_ref() == collider:
+                collider_in_select_group = true
+                break
+        if collider_in_select_group and collider.target == null:
+            stuck_position = null
+            move_timer.start(MOVE_TIMEOUT)
+            return
+    stuck_position = position
+    move_timer.start(MOVE_TIMEOUT)
+
+func navigate_to(destination):
+    target = null
+    path = nav.get_simple_path(position, destination, false)
+
+func set_target():
+    while target == null and not path.empty():
+        var next_node = path[0]
+        path.remove(0)
+        if position.distance_to(next_node) > 9:
+            move_timer.stop()
+            target = next_node
+
+func _on_move_timeout():
+    if (stuck_position != null and position.distance_to(stuck_position) < 1) or stuck_position == null:
+        stuck_position = null
+        target = null
+        path = []
+
+func select(group):
+    selected = true
+    select_group = group
+    select_ring.visible = true
+
+func deselect():
+    selected = false
+    select_ring.visible = false
